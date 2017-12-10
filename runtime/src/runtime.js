168ccd9fd6cd7c67f04d5d2b31307bd594456795
Oscar Spencer <oscar.spen@gmail.com>
2017-12-10 13:53:25 -0500

    Clean up main runtime file. Removed all remnants of the example site.

--- runtime/src/runtime.js
+++ runtime/src/runtime.js
@@ -1,5 +1,7 @@
-import { heapAdjust } from './core/heap';
+import { heapAdjust, grainCheckMemory } from './core/heap';
 import { printClosure } from './core/closures';
+import { throwGrainError } from './errors/errors';
+import { grainToJSVal } from './utils/utils';
 
 import { print, debugPrint } from './lib/print';
 import { equal } from './lib/equal';
@@ -7,19 +9,12 @@ import toString from './lib/to-string';
 import * as libStrings from './lib/strings';
 import * as libDOM from './lib/DOM';
 
-let grainInitialized = false;
 let grainModule;
 
 export const memory = new WebAssembly.Memory({initial: 1});
 export const view = new Int32Array(memory.buffer);
 export const encoder = new TextEncoder("utf-8");
 export const decoder = new TextDecoder("utf-8");
-
-
-
-function displayOnPage(str) {
-  document.getElementById('output').innerText = str;
-}
 
 const importObj = {
   console: {
@@ -41,73 +36,22 @@ const importObj = {
   }
 };
 
-function fetchSource(url) {
-  return fetch(url)
-    .then(response => response.text())
-    .then(code => {
-      document.getElementById('sourceCode').innerText = code;
-      Prism.highlightAll();
-    });
-}
-
-function fetchAndInstantiate(url, importObject) {
-  return fetch(url).then(response => response.arrayBuffer())
-    .then(bytes => WebAssembly.instantiate(bytes, importObject))
-    .then(results => results);
+async function fetchAndInstantiate(url, importObject) {
+  let response = await fetch(url);
+  if (!response.ok) throw new Error(`[Grain] Could not load ${url} due to a network error.`);
+  let bytes = await response.arrayBuffer();
+  return WebAssembly.instantiate(bytes, importObject);
 }
 
 function runGrain(module) {
   grainModule = module;
-  grainInitialized = true;
   let main = module.instance.exports["GRAIN$MAIN"];
   heapAdjust = module.instance.exports["GRAIN$HEAP_ADJUST"];
   let res = main();
-  console.log(`result: ${res}`);
-  let resJS = grainToJSVal(res);
-  printNumber(res);
-  return resJS;
+  return grainToJSVal(res);
 }
 
-function showError(e) {
-  displayOnPage(`[[ERROR: ${e.message}]]`);
-  console.error(e.message);
-  console.error(e.stack);
-  throw e;
+export default async function GrainRunner(uri) {
+  let module = await fetchAndInstantiate(uri, importObj);
+  return runGrain(module);
 }
-
-var examples = {
-  addition: { source: "adder.gr", wasm: "adder.wasm" },
-  lambda: { source: "lambda.gr", wasm: "lambda.wasm" },
-  dom: { source: "domSimple.gr", wasm: "domSimple.wasm" },
-  domCb: { source: "dom.gr", wasm: "dom.wasm" }
-};
-
-function resetPage() {
-  document.getElementById('div1').innerHTML = "";
-  document.getElementById('div2').innerHTML = "";
-  document.getElementById('innerDiv').innerHTML = "";
-  document.getElementById('sourceCode').innerHTML = "";
-}
-
-export default function loadExample(e) {
-  resetPage();
-  fetchSource("examples/".concat(e.source));
-  return fetchAndInstantiate("examples/".concat(e.wasm), importObj)
-    .then(runGrain)
-    .catch(showError);
-}
-
-function makeExampleLoader(e) {
-  return () => loadExample(e);
-}
-
-document.getElementById("navAdd")
-  .addEventListener("click", makeExampleLoader(examples.addition));
-document.getElementById("navFunc")
-  .addEventListener("click", makeExampleLoader(examples.lambda));
-document.getElementById("navDOM")
-  .addEventListener("click", makeExampleLoader(examples.dom));
-document.getElementById("navDOMCallback")
-  .addEventListener("click", makeExampleLoader(examples.domCb));
-
-loadExample(examples.addition);
