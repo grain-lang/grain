828a97cb979e241dd2c84f83202b3d65aa45c297
Oscar Spencer <oscar.spen@gmail.com>
2017-12-10 12:26:01 -0500

    Moved tags to their own file

---
+++ runtime/src/core/tags.js
@@ -0,0 +1,52 @@
+import { memory, view } from '../runtime';
+import { throwGrainError } from '../errors/errors';
+import {
+  GRAIN_ERR_NOT_NUMBER_GENERIC,
+  GRAIN_ERR_NOT_BOOLEAN_GENERIC,
+  GRAIN_ERR_NOT_TUPLE_GENERIC,
+  GRAIN_ERR_NOT_LAMBDA_GENERIC,
+  GRAIN_ERR_NOT_STRING_GENERIC,
+  GRAIN_ERR_NOT_DOM_ELEMENT_GENERIC
+} from '../errors/error-codes';
+
+export const GRAIN_NUMBER_TAG_MASK = 0b0001;
+export const GRAIN_TUPLE_TAG_MASK = 0b0111;
+
+export const GRAIN_NUMBER_TAG_TYPE       = 0b0000;
+export const GRAIN_BOOLEAN_TAG_TYPE      = 0b1111;
+export const GRAIN_TUPLE_TAG_TYPE        = 0b0001;
+export const GRAIN_LAMBDA_TAG_TYPE       = 0b0101;
+export const GRAIN_GENERIC_HEAP_TAG_TYPE = 0b0011;
+
+export const GRAIN_STRING_HEAP_TAG = 1;
+export const GRAIN_DOM_ELEM_TAG = 2;
+
+function getAndMask(tag) {
+  switch(tag) {
+  case GRAIN_NUMBER_TAG_TYPE:
+    return GRAIN_NUMBER_TAG_MASK;
+  default:
+    return GRAIN_TUPLE_TAG_MASK;
+  }
+}
+
+function assertGrainTag(tag, n, err) {
+  if ((n & getAndMask(tag)) !== tag) {
+    throwGrainError(err, n, 0);
+  }
+}
+
+function assertGrainHeapTag(tag, n, err) {
+  assertGrainTag(GRAIN_GENERIC_HEAP_TAG_TYPE, n, err);
+  let ptr = n ^ GRAIN_GENERIC_HEAP_TAG_TYPE;
+  if (view[ptr / 4] != tag) {
+    throwGrainError(err, n, 0);
+  }
+}
+
+export const assertNumber = (n, err) => assertGrainTag(GRAIN_NUMBER_TAG_TYPE, n, err || GRAIN_ERR_NOT_NUMBER_GENERIC);
+export const assertBoolean = (n, err) => assertGrainTag(GRAIN_BOOLEAN_TAG_TYPE, n, err || GRAIN_ERR_NOT_BOOLEAN_GENERIC);
+export const assertTuple = (n, err) => assertGrainTag(GRAIN_TUPLE_TAG_TYPE, n, err || GRAIN_ERR_NOT_TUPLE_GENERIC);
+export const assertLambda = (n, err) => assertGrainTag(GRAIN_LAMBDA_TAG_TYPE, n, err || GRAIN_ERR_NOT_LAMBDA_GENERIC);
+export const assertString = (n, err) => assertGrainHeapTag(GRAIN_STRING_HEAP_TAG, n, err || GRAIN_ERR_NOT_STRING_GENERIC);
+export const assertDOMElement = (n, err) => assertGrainHeapTag(GRAIN_DOM_ELEM_TAG, n, err || GRAIN_ERR_NOT_DOM_ELEMENT_GENERIC);
