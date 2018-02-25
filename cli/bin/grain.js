4efb1e0f6edc2ca2e54c20f4cfd059df1b0c6671
Oscar Spencer <ospencer@kyruus.com>
2018-02-25 16:23:23 -0500

    Add grain cli tool

---
+++ cli/bin/grain.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node --harmony
+
+let program = require('commander');
+let compile = require('./compile.js');
+let run = require('./run.js');
+
+let file;
+
+program
+  .version('Grain compiler 0.0.0\nGrain cli 0.0.0', '-v, --version')
+  .arguments('<file>')
+  .action((givenFile) => {
+    file = givenFile || '';
+  });
+
+program.on('--help', () => {
+  console.log('\n\n  File can be either a Grain (.gr) or WebAssembly (.wasm) file.\n');
+});
+  
+program.parse(process.argv);
+
+let wasmFile;
+if (file.endsWith('gr')) {
+  wasmFile = compile(file);
+} else if (file.endsWith('wasm')) {
+  wasmFile = file;
+} else {
+  program.help();
+}
+
+run(wasmFile);
