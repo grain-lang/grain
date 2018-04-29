14c7784c818afbde38944df640e588133ac2302a
Philip Blair <philip@pblair.org>
2018-04-29 15:17:33 -0400

    Update JS runtime to latest Grain module format

--- cli/bin/compile.js
+++ cli/bin/compile.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process');
 
 module.exports = (file) => {
   try {
-    execSync(`grainc -I src/grain-stdlib ${file}`);
+    execSync(`grainc -I _build/install/default/lib/grain/stdlib ${file}`);
     return file.replace(/\.gr$/, '.wasm')
   } catch (e) {
     console.log(e.stdout.toString());
--- runtime/src/index.js
+++ runtime/src/index.js
@@ -1,6 +1,7 @@
-import GrainRunner from './runtime';
+import {buildGrainRunner, defaultURLLocator} from './runtime';
 if (window.GrainRunner) {
   throw new Error('[Grain] Only one instance of the Grain runtime is allowed!');
 }
+let GrainRunner = buildGrainRunner(defaultURLLocator(window.GrainStdlibRoot));
 window.GrainRunner = GrainRunner;
 export default GrainRunner;
