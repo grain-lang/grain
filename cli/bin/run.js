700cfacf657c42c3db47ce5e22f2260813b99534
Philip Blair <philip@pblair.org>
2018-08-04 11:51:19 -0400

    Move trim()

--- cli/bin/run.js
+++ cli/bin/run.js
@@ -1,8 +1,8 @@
 const { execSync } = require('child_process');
 
 let runtime = require('../../runtime/dist/grain-runtime.js');
-let grainRoot = execSync('grain-root').toString('utf8');
-let locator = runtime.defaultFileLocator(`${grainRoot.trim()}/lib/grain/stdlib`);
+let grainRoot = execSync('grain-root').toString('utf8').trim();
+let locator = runtime.defaultFileLocator(`${grainRoot}/lib/grain/stdlib`);
 let GrainRunner = runtime.buildGrainRunner(locator);
 
 module.exports = async function run(path) {
