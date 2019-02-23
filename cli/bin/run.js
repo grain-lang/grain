29786110bb1216a583169a3e9e9358fd080d9655
Oscar Spencer <oscar@tidelift.com>
2019-02-23 15:44:48 -0500

    Allow multiple paths for module lookup

--- cli/bin/run.js
+++ cli/bin/run.js
@@ -2,11 +2,11 @@ const { execSync } = require('child_process');
 
 let runtime = require('../../runtime/dist/grain-runtime.js');
 let grainRoot = execSync('grain-root').toString('utf8').trim();
-let locator = runtime.defaultFileLocator(`${grainRoot}/lib/grain/stdlib`);
-let GrainRunner = runtime.buildGrainRunner(locator);
 
 module.exports = async function run(path, options) {
   try {
+    let locator = runtime.defaultFileLocator(options.includeDirs.concat(`${grainRoot}/lib/grain/stdlib`));
+    let GrainRunner = runtime.buildGrainRunner(locator);
     let result = await GrainRunner.runFile(path);
     if (options.printOutput) {
       console.log(result);
