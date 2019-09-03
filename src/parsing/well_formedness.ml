0d504cbd5f8220c322a30fd8f6f544c0ce28929b
Oscar Spencer <oscar@tidelift.com>
2019-09-03 00:20:59 -0400

    Add well-formedness rules around imports.

--- src/parsing/well_formedness.ml
+++ src/parsing/well_formedness.ml
@@ -6,6 +6,10 @@ type wferr =
   | MalformedString of Location.t
   | MultipleModuleName of Location.t
   | TypeNameShouldBeUppercase of string * Location.t
+  | IllegalAliasName of string * Location.t
+  | ExternalAlias of string * Location.t
+  | ModuleNameShouldBeUppercase of string * Location.t
+  | ModuleImportNameShouldNotBeExternal of string * Location.t
   | TyvarNameShouldBeLowercase of string * Location.t
   | ExportAllShouldOnlyAppearOnce of Location.t
 
@@ -21,6 +25,14 @@ let prepare_error =
     errorf ~loc "Multiple modules in identifier"
   | TypeNameShouldBeUppercase(name, loc) ->
     errorf ~loc "Type '%s' should have an uppercase name." name
+  | IllegalAliasName(name, loc) ->
+    errorf ~loc "Alias '%s' should have proper casing." name
+  | ExternalAlias(name, loc) ->
+    errorf ~loc "Alias '%s' should be at most one level deep." name
+  | ModuleNameShouldBeUppercase(name, loc) ->
+    errorf ~loc "Module '%s' should have an uppercase name." name
+  | ModuleImportNameShouldNotBeExternal(name, loc) ->
+    errorf ~loc "Module name '%s' should contain only one module." name
   | TyvarNameShouldBeLowercase(var, loc) ->
     errorf ~loc "Type variable '%s' should be lowercase." var
   | ExportAllShouldOnlyAppearOnce loc ->
@@ -68,7 +80,28 @@ let malformed_identifiers errs super =
       | _ -> ()
     end;
     super.expr self e in
-  let iterator = { super with expr = iter_expr } in
+  let iter_import self imports =
+    let xor p q = (p && (not q)) || ((not p) && q) in
+    let casing_mismatch orig alias =
+      let o = orig.[0] in
+      let a = alias.[0] in
+      xor (BatChar.is_uppercase o) (BatChar.is_uppercase a) in
+    List.iter (fun {pimp_val} ->
+      match pimp_val with
+      | PImportValues values ->
+        List.iter (fun (name, alias) ->
+          match (name, alias) with
+          | {txt=IdentName(orig)}, Some({txt=IdentName(alias); loc}) 
+            when casing_mismatch orig alias -> 
+            errs := (IllegalAliasName (alias, loc))::!errs
+          | _, Some({txt=(IdentExternal _ as alias); loc}) ->
+            errs := (ExternalAlias (Identifier.string_of_ident alias, loc))::!errs
+          | _ -> ()
+        ) values
+      | _ -> ()
+    ) imports;
+    super.import self imports in
+  let iterator = { super with expr = iter_expr; import = iter_import } in
   { errs; iterator }
 
 let types_have_correct_case errs super =
@@ -81,6 +114,40 @@ let types_have_correct_case errs super =
     super.data self d in
   (* FIXME: The parser should read in uppercase types as PTyConstr instances *)
   let iterator = { super with data = iter_data } in
+  { errs; iterator }
+
+let modules_have_correct_case errs super =
+  let check_uppercase loc s =
+    let first_char = String.get s 0 in
+    if first_char <> BatChar.uppercase first_char then
+      errs := (ModuleNameShouldBeUppercase(s, loc))::!errs in
+  let iter_mods self imports =
+    List.iter (fun {pimp_mod_alias=alias} ->
+      match alias with
+      | Some {loc=name_loc; txt=(IdentName name)} ->
+        check_uppercase name_loc name;
+      | Some _ (* IdentExternal handled by another WF rule *)
+      | None -> ()
+    ) imports;
+    super.import self imports in
+  let iterator = { super with import = iter_mods } in
+  { errs; iterator }
+
+let module_imports_not_external errs super =
+  let check_name loc id =
+    match id with
+    | Identifier.IdentName _ -> ()
+    | Identifier.IdentExternal _ ->
+      errs := (ModuleImportNameShouldNotBeExternal(Identifier.string_of_ident id, loc))::!errs in
+  let iter_mods self imports =
+    List.iter (fun {pimp_mod_alias=alias} ->
+      match alias with
+      | Some {loc=name_loc; txt=name} ->
+        check_name name_loc name;
+      | None -> ()
+    ) imports;
+    super.import self imports in
+  let iterator = { super with import = iter_mods } in
   { errs; iterator }
 
 let only_has_one_export_all errs super =
@@ -103,6 +170,8 @@ let well_formedness_checks = [
   malformed_strings;
   malformed_identifiers;
   types_have_correct_case;
+  modules_have_correct_case;
+  module_imports_not_external;
   only_has_one_export_all;
 ]
 
