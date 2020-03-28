2027e7bf61a6b7abdbc7b7953f853a666eb87150
Oscar Spencer <oscar@tidelift.com>
2020-03-27 21:48:30 -0400

    Add support files for basic types for assemblyscript

---
+++ stdlib/ascutils/tags.ts
@@ -0,0 +1,14 @@
+export const GRAIN_NUMBER_TAG_TYPE: i32       = 0b0000;
+export const GRAIN_CONST_TAG_TYPE: i32        = 0b1111;
+export const GRAIN_TUPLE_TAG_TYPE: i32        = 0b0001;
+export const GRAIN_LAMBDA_TAG_TYPE: i32       = 0b0101;
+export const GRAIN_GENERIC_HEAP_TAG_TYPE: i32 = 0b0011;
+
+export const GRAIN_NUMBER_TAG_MASK: i32 = 0b0001;
+export const GRAIN_GENERIC_TAG_MASK: i32 = 0b0111;
+
+export const GRAIN_STRING_HEAP_TAG: i32 = 1;
+export const GRAIN_DOM_ELEM_TAG: i32 = 2;
+export const GRAIN_ADT_HEAP_TAG: i32 = 3;
+export const GRAIN_RECORD_HEAP_TAG: i32 = 4;
+export const GRAIN_ARRAY_HEAP_TAG: i32 = 5;
