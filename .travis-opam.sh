50944739c8f93b0e0598e50e5b2fb7629a1e068e
Oscar Spencer <oscar@tidelift.com>
2020-04-02 00:54:29 -0400

    Remove old license information

--- .travis-opam.sh
+++ .travis-opam.sh
@@ -1,19 +1,3 @@
-# This script is from https://github.com/ocaml/ocaml-ci-scripts
-
-## ISC License
-
-# Permission to use, copy, modify, and distribute this software for any
-# purpose with or without fee is hereby granted, provided that the above
-# copyright notice and this permission notice appear in all copies.
-
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
 echo -en "travis_fold:start:prepare.ci\r"
 
 ### Setup Grain runtime and cli
