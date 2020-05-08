1bbd60e0936988d410b80905e11edba245b2b17e
Oscar Spencer <oscar@tidelift.com>
2020-05-07 20:03:33 -0400

    Add link to issue about experimental-wasm-bigint

--- cli/bin/grain.js
+++ cli/bin/grain.js
@@ -1,5 +1,7 @@
 #!/bin/sh
 // 2>/dev/null; exec /usr/bin/env node --experimental-wasm-bigint "$0" "$@"
+// https://github.com/grain-lang/grain/issues/114
+
 const path = require('path');
 
 let program = require('commander');
