f8030d22304d6524beff1b542b329ae500747a28
Blaine Bublitz <blaine.bublitz@gmail.com>
2020-06-28 12:45:27 -0700

    Adds a build script to run assemblyscript

---
+++ stdlib/build.mjs
@@ -0,0 +1,29 @@
+#!/bin/sh
+// 2>/dev/null; exec /usr/bin/env node --harmony-top-level-await "$0"
+
+import { promisify } from 'util';
+import { createRequire } from 'module';
+
+import globby from 'globby';
+import replaceExt from 'replace-ext';
+
+// Making asc interop with ESM
+const require = createRequire(import.meta.url);
+const asc = require('assemblyscript/cli/asc');
+
+const compileFile = promisify(asc.main);
+
+// Binaryen is ready
+await asc.ready;
+
+const filenames = await globby('stdlib-external/**/*.ts');
+
+const files = filenames.map((filename) => compileFile([
+  filename,
+  '-o', replaceExt(filename, '.wasm'),
+  '-O3',
+  '--runtime', 'none',
+  '--importMemory',
+]));
+
+await Promise.all(files);
