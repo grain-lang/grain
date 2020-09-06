1d0c7965ed07d8a4791675e3e89bd8bc04ba763e
Oscar Spencer <oscar@tidelift.com>
2020-09-06 18:16:00 -0400

    Redefine String concat as ++

--- stdlib/stdlib-external/string.ts
+++ stdlib/stdlib-external/string.ts
@@ -28,21 +28,6 @@ export function byteLength(s: u32): u32 {
   return size << 1
 }
 
-export function concat(s1: u32, s2: u32): u32 {
-  s1 = s1 ^ GRAIN_GENERIC_HEAP_TAG_TYPE
-  s2 = s2 ^ GRAIN_GENERIC_HEAP_TAG_TYPE
-
-  const size1 = stringSize(s1)
-  const size2 = stringSize(s2)
-
-  const newString = allocateString(size1 + size2)
-
-  memory.copy(newString + 8, s1 + 8, size1)
-  memory.copy(newString + 8 + size1, s2 + 8, size2)
-
-  return newString ^ GRAIN_GENERIC_HEAP_TAG_TYPE
-}
-
 export function slice(s: u32, from: i32, to: i32): u32 {
   const len = i32(length(s) >> 1)
 
