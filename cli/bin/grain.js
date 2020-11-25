4ea26021d270adc542736efa90093d2f2cdbef5b
Blaine Bublitz <blaine.bublitz@gmail.com>
2020-11-25 09:59:12 -0700

    fix: replace shebang trick with flagged-respawn to support Windows (#430)

--- cli/bin/grain.js
+++ cli/bin/grain.js
@@ -1,71 +1,91 @@
-#!/bin/sh
-// 2>/dev/null; exec /usr/bin/env node --experimental-wasm-bigint "$0" "$@"
-// https://github.com/grain-lang/grain/issues/114
+#!/usr/bin/env node
 
-const { execSync } = require('child_process');
-const path = require('path');
+const v8flags = require('v8flags');
+const flaggedRespawn = require('flagged-respawn');
 
-let program = require('commander');
-let compile = require('./compile.js');
-let run = require('./run.js');
-let lsp = require('./lsp.js');
+v8flags(onFlags);
 
-let pervasivesPath = require.resolve('@grain/stdlib');
-let stdlibPath = path.dirname(pervasivesPath);
+function onFlags(err, flags) {
+  if (err) {
+    console.error('Encountered error when retrieving v8 flags: ', err);
+    process.exit(1);
+  }
 
-function list(val) {
-  return val.split(',');
+  // https://github.com/grain-lang/grain/issues/114
+  flaggedRespawn(flags, process.argv, ['--experimental-wasm-bigint'], onRespawn);
 }
 
-function num(val) {
-  return Number.parseInt(val, 10);
+function onRespawn(ready, proc, argv) {
+  if (!ready) {
+    // Respawning
+    return;
+  }
+
+  const { execSync } = require('child_process');
+  const path = require('path');
+
+  let program = require('commander');
+  let compile = require('./compile.js');
+  let run = require('./run.js');
+  let lsp = require('./lsp.js');
+
+  let pervasivesPath = require.resolve('@grain/stdlib');
+  let stdlibPath = path.dirname(pervasivesPath);
+
+  function list(val) {
+    return val.split(',');
+  }
+
+  function num(val) {
+    return Number.parseInt(val, 10);
+  }
+
+  function graincVersion() {
+    const grainc = path.join(__dirname, 'grainc.exe');
+
+    return execSync(`${grainc} --version`).toString().trim();
+  }
+
+  program
+    .option('-v, --version', 'output CLI and compiler versions')
+    .on('option:version', () => {
+      console.log(`Grain cli ${require('../package.json').version}`)
+      console.log(`Grain compiler ${graincVersion()}`)
+      process.exit(0)
+    })
+    .description('Compile and run Grain programs. 🌾')
+    .option('-p, --print-output', 'print the output of the program')
+    .option('-g, --graceful', 'return a 0 exit code if the program errors')
+    .option('-I, --include-dirs <dirs>', 'include directories the runtime should find wasm modules', list, [])
+    .option('-f, --cflags <cflags>', 'pass flags to the Grain compiler')
+    .option('-S, --stdlib <path>', 'override the standard libary with your own', stdlibPath)
+    .option('--limitMemory <size>', 'maximum allowed heap size', num, -1)
+    // The root command that compiles & runs
+    .arguments('<file>')
+    .action(function (file) {
+      run(compile(file, program), program);
+    })
+
+  program
+    .command('compile <file>')
+    .description('compile a grain program into wasm')
+    .action(function (file) {
+      compile(file, program);
+    });
+
+  program
+    .command('lsp <file>')
+    .description('check a grain file for LSP')
+    .action(function (file) {
+      lsp(file, program);
+    });
+
+  program
+    .command('run <file>')
+    .description('run a wasm file with the grain runtime')
+    .action(function (wasmFile) {
+      run(wasmFile, program);
+    });
+
+  program.parse(argv);
 }
-
-function graincVersion() {
-  const grainc = path.join(__dirname, 'grainc.exe');
-
-  return execSync(`${grainc} --version`).toString().trim();
-}
-
-program
-  .option('-v, --version', 'output CLI and compiler versions')
-  .on('option:version', () => {
-    console.log(`Grain cli ${require('../package.json').version}`)
-    console.log(`Grain compiler ${graincVersion()}`)
-    process.exit(0)
-  })
-  .description('Compile and run Grain programs. 🌾')
-  .option('-p, --print-output', 'print the output of the program')
-  .option('-g, --graceful', 'return a 0 exit code if the program errors')
-  .option('-I, --include-dirs <dirs>', 'include directories the runtime should find wasm modules', list, [])
-  .option('-f, --cflags <cflags>', 'pass flags to the Grain compiler')
-  .option('-S, --stdlib <path>', 'override the standard libary with your own', stdlibPath)
-  .option('--limitMemory <size>', 'maximum allowed heap size', num, -1)
-  // The root command that compiles & runs
-  .arguments('<file>')
-  .action(function (file) {
-    run(compile(file, program), program);
-  })
-
-program
-  .command('compile <file>')
-  .description('compile a grain program into wasm')
-  .action(function (file) {
-    compile(file, program);
-  });
-
-program
-  .command('lsp <file>')
-  .description('check a grain file for LSP')
-  .action(function (file) {
-    lsp(file, program);
-  });
-
-program
-  .command('run <file>')
-  .description('run a wasm file with the grain runtime')
-  .action(function (wasmFile) {
-    run(wasmFile, program);
-  });
-
-program.parse(process.argv);
