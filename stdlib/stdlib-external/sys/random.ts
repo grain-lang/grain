a53828398be1e5b5344a3a94303b30099e51a7b8
Oscar Spencer <oscar@tidelift.com>
2020-12-16 19:04:20 -0500

    Can't use memory.data

--- stdlib/stdlib-external/sys/random.ts
+++ stdlib/stdlib-external/sys/random.ts
@@ -1,17 +1,19 @@
-import { throwError } from "../ascutils/grainRuntime"
+import { throwError, malloc, free } from "../ascutils/grainRuntime"
 
 import { GRAIN_ERR_SYSTEM } from "../ascutils/errors"
 
 import { errno, random_get } from "bindings/wasi"
 
 export function random(): u32 {
-  let buf = memory.data(4)
+  let buf = malloc(4)
 
   let err = random_get(buf, 4)
   if (err !== errno.SUCCESS) {
+    free(buf)
     throwError(GRAIN_ERR_SYSTEM, err << 1, 0)
   }
 
   let rand = load<u32>(buf)
+  free(buf)
   return rand << 1
 }
