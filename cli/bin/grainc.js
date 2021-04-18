06e9192cd2c1facbab3ecb27eb4595fda517b6dd
Blaine Bublitz <blaine.bublitz@gmail.com>
2021-04-17 19:29:40 -0700

    stdlib cleanup

--- cli/bin/grainc.js
+++ cli/bin/grainc.js
@@ -1,10 +1,5 @@
 // A wrapper around grainc_js.bc.js that prepares the `pkg` env
-const program = require("commander");
 const preparePkg = require("./pkg");
-
-program.option("--stdlib <path>").allowUnknownOption().parse(process.argv);
-
-const { stdlib } = program.opts();
 
 preparePkg();
 
