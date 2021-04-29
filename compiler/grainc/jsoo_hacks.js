627a972407cc8b630ed7d41ccfe531f8b1c9de94
Blaine Bublitz <blaine.bublitz@gmail.com>
2021-04-28 21:15:38 -0700

    fix: Workaround for Windows drive letters with jsoo & pkg

--- compiler/grainc/jsoo_hacks.js
+++ compiler/grainc/jsoo_hacks.js
@@ -1,6 +1,12 @@
 //Provides: caml_make_path
 //Requires: caml_current_dir
 //Requires: caml_jsstring_of_string
+//Requires: caml_root, jsoo_mount_point, MlNodeDevice
+if (caml_root.charCodeAt(1) == 58 && caml_root[0] != 'C') {
+  // This ensure we can find our C:/snapshot/ stuff even when running in a diff drive
+  // Not the ideal place to do this but it is the easiest to avoid free variables
+  jsoo_mount_point.push({ path: 'C:/', device: new MlNodeDevice('C:/') });
+}
 function caml_make_path(name) {
   name = caml_jsstring_of_string(name);
   // We needed to provide our own `caml_make_path` because it doesn't check
