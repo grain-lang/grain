56eb90b2b17c77bf827ab93ac14433781a1ab6a2
Blaine Bublitz <blaine.bublitz@gmail.com>
2021-08-29 15:38:34 -0700

    chore(js-runner): Upgrade webpack & babel

--- js-runner/webpack.prod.js
+++ js-runner/webpack.prod.js
@@ -1,5 +1,5 @@
 const webpack = require("webpack");
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = common.map((config) =>
