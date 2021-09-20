1cc0c5c03d38957c90f71e99a188c448923a481c
Blaine Bublitz <blaine.bublitz@gmail.com>
2021-09-20 10:13:37 -0700

    fix(cli): Only add --experimental-wasm-bigint flag on node 14 or 15 (#899)

--- cli/bin/grain.js
+++ cli/bin/grain.js
@@ -10,7 +10,12 @@ const v8 = require("v8");
  *
  * This seems to work for our needs with Node 14, but we should be cautious when updating.
  */
-v8.setFlagsFromString("--experimental-wasm-bigint");
+if (
+  process.versions.node.startsWith("14.") ||
+  process.versions.node.startsWith("15.")
+) {
+  v8.setFlagsFromString("--experimental-wasm-bigint");
+}
 v8.setFlagsFromString("--experimental-wasm-return-call");
 
 const program = require("commander");
