b8bb6736a4c00058704352dffb6d43a753d040d2
jozanza <hello@jsavary.com>
2022-05-24 21:30:31 -0400

    Make jest and near-workspaces optional

--- cli/__test__/near.spec.js
+++ cli/__test__/near.spec.js
@@ -1,24 +1,26 @@
 const path = require("path");
-const { Worker } = require("near-workspaces");
 
 // NOTE:
 // This test suite exists to ensure wasm output does not contain multivalue functions.
 // If no multivalue functions exist, Grain smart contracts should run without issue.
 
 let worker;
-
-beforeAll(async () => {
-  worker = await Worker.init();
-});
-
 afterAll(async () => {
-  await worker.tearDown();
+  await worker?.tearDown();
 });
 
 describe("Runs in NEAR sandbox", () => {
   jest.setTimeout(60_000);
-
-  test("it should not produce wasm multivalue function", async () => {
+  let Worker;
+  try {
+    Worker = require("near-workspaces").Worker;
+  } catch (err) {
+    // no-op
+  }
+  // near-workspaces is optional, so if it's not installed, we can skip tests
+  const testIf = !Worker ? test.skip : test;
+  testIf("it should not produce wasm multivalue function", async () => {
+    worker = await Worker.init();
     const root = worker.rootAccount;
     const contract = await root.createAndDeploy(
       "grain-near-multivalue-test",
