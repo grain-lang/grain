251f991318a866ee54122b81d898009f7eb93966
Spotandjake <spotandjake@hotmail.com>
2026-09-16 16:08:14 -0400

    feat(cli)!: Switch to node-sea and move to node 24
    This pr switches our compiler from `yao-pkg/pkg`s classic build mode to their `node-sea` based mode.
    
    Benefits:
    * The security concern of malicious binaries has been negated as we are now using the offical node binaries
    * Upgrading node versions is now a simple process that doesn't require waiting on pkg-fetch builds
    
    Currently this pr is using my fork of `pkg` as I needed to expose the internal vfs in order to implement our write hack for the stdlib. I am going to try to upstream this or we can move to using grain's pkg fork.
    
    Breaking:
    I'm not sure if we actually consider this pr breaking or not, because of node-sea requirements we are dropping support for node 22 completely.

--- cli/bin/pkg.js
+++ cli/bin/pkg.js
@@ -2,26 +2,107 @@
 if (process.pkg) {
   const fs = require("fs");
   const path = require("path");
-  const stdlibPath = require("@grain/stdlib");
-  const stdlibPathWithTrailingSep = `${stdlibPath}${path.sep}`;
-  // From https://github.com/sindresorhus/escape-string-regexp/blob/main/index.js
-  const regexpSafeStdlibPath = stdlibPathWithTrailingSep
-    .replace(/[|\\{}()[\]^$+*?.]/g, "\\$&")
-    .replace(/-/g, "\\x2d");
+  const stdlib = require("@grain/stdlib");
+  const stdlibPath = fs.realpathSync(stdlib);
+  const vfs = process.pkg.vfs;
 
-  const stdlibTarget = path.join(process.cwd(), "/target");
+  // NOTE: pkg's SeaProvider lazily reads files outside its VFS, but doesn't
+  // support file descriptor APIs, so we load the files into the VFS ourselves.
+  const copyGrainFiles = (dir) => {
+    for (const entry of fs.readdirSync(dir)) {
+      const file = path.join(dir, entry);
+      const stats = fs.statSync(file);
+      if (stats.isDirectory()) {
+        copyGrainFiles(file);
+      } else if (stats.isFile() && path.extname(file) === ".gr") {
+        const content = fs.readFileSync(file);
+        vfs.writeFileSync(file, content);
+      }
+    }
+  };
+  copyGrainFiles(stdlibPath);
 
-  try {
-    fs.mkdirSync(stdlibTarget, { recursive: true });
-  } catch (err) {
-    console.error(`Unable to create directory: ${stdlibTarget}`, err);
-    process.exit(1);
-  }
-
-  process.pkg.mount(
-    new RegExp(`${regexpSafeStdlibPath}(.+)(?<!.gr)$`),
-    function (_match, group1) {
-      return path.join(stdlibTarget, group1);
-    },
+  const targetDirectory = path.join(
+    process.cwd(),
+    `target${path.sep}stdlib${path.sep}`,
   );
+
+  // Helpers to determine if we are working with the packaged grain stdlib
+  const isSubDirectory = (parent, child) => {
+    const relative = path.relative(parent, child);
+    return (
+      relative !== "" &&
+      !relative.startsWith("..") &&
+      !path.isAbsolute(relative)
+    );
+  };
+  const isInStdlib = (p) =>
+    isSubDirectory(stdlib, p) || isSubDirectory(stdlibPath, p);
+  const isGrainFile = (p) => path.extname(p) === ".gr";
+
+  // Patch the fs module to redirect calls to the stdlib to the virtual filesystem
+  // NOTE: While the `pkg` vfs does this itself it doesn't handle file descriptors correctly, so we need to do that ourselves
+  const FS_NAMES = [
+    "readFileSync",
+    "readFile",
+    "writeFileSync",
+    "writeFile",
+    "appendFileSync",
+    "appendFile",
+    "openSync",
+    "open",
+    "statSync",
+    "stat",
+    "lstatSync",
+    "lstat",
+    "readdirSync",
+    "readdir",
+    "existsSync",
+    "accessSync",
+    "access",
+    "mkdirSync",
+    "mkdir",
+    "rmSync",
+    "rm",
+    "unlinkSync",
+    "unlink",
+    "realpathSync",
+    "realpath",
+    "createReadStream",
+    "createWriteStream",
+    "fstat",
+    "fstatSync",
+    "closeSync",
+    "readSync",
+  ];
+  const virtualFileDescriptors = new Set();
+  for (const name of FS_NAMES) {
+    const originalFunc = fs[name];
+    if (typeof originalFunc !== "function") continue;
+    fs[name] = function (arg0, ...args) {
+      if (typeof arg0 == "string" && isInStdlib(arg0)) {
+        if (isGrainFile(arg0)) {
+          const result = vfs[name](arg0, ...args);
+          if (name === "openSync" || name === "open")
+            virtualFileDescriptors.add(result);
+          return result;
+        } else {
+          // Remap stdlib paths to target directory so that we can write to them
+          const relativePath = path.relative(
+            path.resolve(stdlibPath),
+            path.resolve(arg0),
+          );
+          const newPath = path.join(targetDirectory, relativePath);
+          if (!fs.existsSync(targetDirectory))
+            fs.mkdirSync(targetDirectory, { recursive: true });
+          return originalFunc.call(fs, newPath, ...args);
+        }
+      } else if (typeof arg0 == "number" && virtualFileDescriptors.has(arg0)) {
+        if (name === "closeSync" || name === "close")
+          virtualFileDescriptors.delete(arg0);
+        return vfs[name](arg0, ...args);
+      }
+      return originalFunc.call(fs, arg0, ...args);
+    };
+  }
 }
